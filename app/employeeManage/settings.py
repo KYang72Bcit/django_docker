f48eda14baa02457d9c488bfc2900fee06762ab6
KeYang <kyang72@my.bcit.ca>
2022-06-03 13:38:18 -0700

    nothing added

--- app/employeeManage/settings.py
+++ app/employeeManage/settings.py
@@ -128,13 +128,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# 设置STATIC ROOT 和 STATIC URL
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR /'static/'),]
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static_new/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
-# 设置MEDIA ROOT 和 MEDIA URL
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = "/media/"
+# # 设置STATIC ROOT 和 STATIC URL
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = "/static/app01/"
+
+# # 设置MEDIA ROOT 和 MEDIA URL
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = "/media/"
 
 # STATIC_URL = "/static/static/"
 # STATIC_ROOT = "/vol/web/static"
