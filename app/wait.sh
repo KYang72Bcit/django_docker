9bd6aecb967e0fd42d4604f8dac108cc2cd48c7f
KeYang <kyang72@my.bcit.ca>
2022-06-10 17:28:29 -0700

    add google run

--- app/wait.sh
+++ app/wait.sh
@@ -6,7 +6,7 @@ while ! nc -z db 3306 ; do
 done
 
 # python manage.py makemigrations
-# python manage.py migrate
-# python manage.py runserver 0.0.0.0:8000
+ python manage.py migrate
+ # python manage.py runserver 0.0.0.0:8000
 
 exec "$@"
